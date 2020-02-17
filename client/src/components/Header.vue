fd031d5f21bc8d2f35cf594e1667f9b1037d08a4
diegoorellanaga <diegoorellanaga@gmail.com>
2020-02-17 15:42:55 -0500

    add sign up button

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,14 +1,25 @@
 <template>
-  <v-app-bar fixed dense app class="cyan">
+  <v-toolbar fixed dense class="cyan">
     <v-toolbar-title>
       TabTracker
     </v-toolbar-title>
-  </v-app-bar>
+<!--     <v-toolbar-items>
+      <v-btn class="cyan" text>
+        Browser
+      </v-btn>
+    </v-toolbar-items> -->
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn class="cyan" text>
+        <router-link to="register">Sign Up</router-link>
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 <script>
 export default {
 }
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component only v-app-bar  -->
 <style scoped>
 </style>
